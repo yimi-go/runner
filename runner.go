44b1ec5242612f463ad4dae5f9ed2502d7b9a132
dowenliu-xyz <hawkdowen@hotmail.com>
2022-12-06 13:15:44 +0800

    feat: Status and Stateful

--- runner.go
+++ runner.go
@@ -2,10 +2,27 @@ package runner
 
 import "context"
 
+type Status string
+
+func (s Status) String() string {
+	return string(s)
+}
+
+const (
+	StatusUnknown   Status = "UNKNOWN"
+	StatusDown             = "DOWN"
+	StatusPreparing        = "PREPARING"
+	StatusUp               = "UP"
+	StatusStopping         = "STOPPING"
+)
+
+type Stateful interface {
+	// Status reports the runner's status.
+	Status() Status
+}
+
 // Runner is a lifecycle manager that run something.
 type Runner interface {
-	// Prepare prepares the runner.
-	Prepare(ctx context.Context) error
 	// Run starts running this runner.
 	// It returns an error if any error occurred during running.
 	// Note the running may block current goroutine,
